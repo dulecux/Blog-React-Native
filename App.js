c19683c444b414f7e8517e5d0eb7838100d6cc21
dulecux <dusan.culibrk@live.com>
2021-10-28 00:51:23 +0200

    Folder name edited

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import IndexScreen from "./scr/screens/IndexScreen";
-import { BlogProvider } from './scr/context/BlogContext'
+import IndexScreen from "./src/screens/IndexScreen";
+import { BlogProvider } from './src/context/BlogContext'
 
 const navigator = createStackNavigator(
   {
